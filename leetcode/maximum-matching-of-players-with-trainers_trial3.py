3626a64e48330ba45631a3360b3cde000a5e1193
Wanjiru Natalie <134970992+Nataliewanjiru@users.noreply.github.com>
2026-03-07 19:26:01 +0300

    Add solution for Maximum Matching of Players With Trainers (trial 3)

---
+++ leetcode/maximum-matching-of-players-with-trainers_trial3.py
@@ -0,0 +1,17 @@
+class Solution:
+    def matchPlayersAndTrainers(self, players: List[int], trainers: List[int]) -> int:
+        i,j = 0,0
+        players.sort()
+        trainers.sort()
+        
+        count = 0
+
+        while i < len(players) and j < len(trainers):
+            if players[i] <= trainers[j]:
+                count +=1
+                i+=1
+                j+=1
+            else:
+                j += 1
+        
+        return count
