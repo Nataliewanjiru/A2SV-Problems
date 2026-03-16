753aad0cb5632b160de022386102c5689d254a95
Wanjiru Natalie <134970992+Nataliewanjiru@users.noreply.github.com>
2026-03-16 10:15:02 +0300

    Add solution for Maximum Points You Can Obtain from Cards (trial 2)

---
+++ leetcode/maximum-points-you-can-obtain-from-cards_trial2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxScore(self, cardPoints: List[int], k: int) -> int:
+        n = len(cardPoints)
+        window_size = n - k
+        total_sum = sum(cardPoints)
+        
+        if k == n:
+            return total_sum
+        
+        current_window_sum = sum(cardPoints[:window_size])
+        min_window_sum = current_window_sum
+        
+        for i in range(window_size, n):
+            current_window_sum += cardPoints[i] - cardPoints[i - window_size]
+            min_window_sum = min(min_window_sum, current_window_sum)
+            
+        return total_sum - min_window_sum
